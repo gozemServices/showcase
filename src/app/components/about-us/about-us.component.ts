6a2355b2506cd6b70cdb4cf7f11065034b2e8bd2
tiagho <tiaghojeux@gmail.com>
2024-11-25 17:11:00 +0100

    correcting nodejs incompatibility and add fontawesome icons

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram, faLinkedin, faWhatsapp} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
+  faFacebook = faFacebook;
+  faLinkedin = faLinkedin;
+  fawhatsapp = faWhatsapp;
+  fainstagram = faInstagram;
+
+
 
 }
--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faClipboardList, faCoffee, faPeopleArrows, faUserCheck, faUserCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+
+  faPeapleArrow = faPeopleArrows;
+  faClipboardList = faClipboardList;
+  faUserCheck = faUserCheck;
+  faUserCog = faUserCog;
 
 }
