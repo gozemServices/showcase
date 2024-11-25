7716b1289a06bc5b282411a850bf9364958dedd1
tiagho <tiaghojeux@gmail.com>
2024-11-25 17:52:01 +0100

    adding responsiveness

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isOpen = false;
 
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+  }
 }
