034122401e00448dac1f4fb2bf342d02d25fff1f
tiagho <tiaghojeux@gmail.com>
2024-12-05 12:34:57 +0100

    update correction

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -20,9 +20,9 @@ export class AboutUsComponent {
     description : {
       p1 : ` Créé en 2015 par des jeunes très ambitieux, GOZEN SERVICE propose des prestations qui couvrent les 
                         métiers des Ressources Humaines: le Recrutement, la Formation & le Coaching, la mise à disposition du personnel
-                        temporaire et le conseil en organisation. GOZEN s&rsquo;est donné ainsi pour mission d&rsquo;apporter des solutions 
-                        adaptées pour le positionnement et l&rsquo;épanouissement professionnel des individus`,
-      p2 : `Animée par une vision claire de devenir le facilitateur des trajectoires professionnelles, GOZEN SERVICE place l’humain au centre de tous ses processus, en proposant des solutions innovantes et adaptées pour accompagner les entreprises et les talents dans le montage de leurs projets professionnels.`
+                        temporaire et le conseil en organisation. GOZEN s'est donné ainsi pour mission d'apporter des solutions 
+                        adaptées pour le positionnement et l'épanouissement professionnel des individus`,
+      p2 : `Animée par une vision claire de devenir le facilitateur des trajectoires professionnelles, GOZEN SERVICE place l'humain au centre de tous ses processus, en proposant des solutions innovantes et adaptées pour accompagner les entreprises et les talents dans le montage de leurs projets professionnels.`
     },
 
     socials : [
